8ee4cfbac91766d08dda260d9fae4bdf4d996406
Shenuri Sumansekara <shenuriseth@gmail.com>
2021-11-01 19:26:55 +0000

    added fields to the user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number');
+            $table->date('dob');
             $table->rememberToken();
             $table->timestamps();
         });
